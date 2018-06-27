1f8b985bbf2826fd4ad7449e5c9c99e77ffff373
Benson Li <bensonbinbinli@gmail.com>
2018-06-26 17:23:13 -0700

    wrote a threading function to check the mongodb connection instead

--- Logger.py
+++ Logger.py
@@ -7,16 +7,17 @@ import ipdb
 class SummaryWriter(Database):
     def __init__(self,folder_name,run_name=None):
         # super().__init__()
-        # self.client = MongoClient()
-        self.client = safeMongoClient()
+        self.client = MongoClient()
 
         if run_name == None:
             run_name = datetime.datetime.now().strftime("%B %d, %Y at %I:%M%p")
 
         self.experiment = self.client[folder_name][run_name]
+        self.check_connection()
 
         ## convert to better format for regex/datetime comparisons
         self.add_experiment_parameter("Time",run_name)
+        
 
     def add_scalar(self,variable_name:str, f:int, t:int):
         '''
@@ -26,6 +27,7 @@ class SummaryWriter(Database):
 
     def add_experiment_parameter(self,parameter:str, value:int):
         self.experiment.insert({'Experimental Parameters':'Experimental Parameters',parameter:value})
+
         
 
 if __name__ == '__main__':
@@ -34,7 +36,6 @@ if __name__ == '__main__':
     for i in range(5):
         w.add_scalar('Loss',2*i,i)
     w.viewFolder('test')
-    ipdb.set_trace()
     w.close()
     w.removeFolder('test')
 
--- utils.py
+++ utils.py
@@ -1,45 +1,11 @@
 from pymongo import MongoClient
+from threading import Thread
 import ipdb 
-
-def removeFolder(folder_name):
-    client = safeMongoClient()
-    client.drop_database(folder_name)
-    client.close()
-
-def removeRun(folder_name,run_name):
-    client = safeMongoClient()
-    client[folder_name][run_name].drop()
-    client.close()
-
-def viewFolder(folder_name):
-    '''
-    show all collections in a folder
-    '''
-    client = safeMongoClient()
-    # include include_system_collections=False?
-    for collection in client[folder_name].collection_names():
-        print(collection)
-    client.close()
-def viewRun(folder_name,run_name):
-    '''
-    show all documents in a collection
-    '''
-    client = safeMongoClient()
-    for doc in client[folder_name][run_name].find():
-        print(doc)
-    client.close()
-
-def safeMongoClient():
-    client = MongoClient()
-    ## won't work because client is lazy loaded 
-    if client== None:
-        print("client is NoneType")
-        raise "Cannot connect to MongoDB!"
-    return client
 
 class Database():
     def __init__(self):
         self.client = safeMongoClient()
+    ## Database utilities
     def removeFolder(self,folder_name):
         self.client.drop_database(folder_name)
 
@@ -61,3 +27,22 @@ class Database():
             print(doc)
     def close(self):
         self.client.close()
+    ## Connection utilties
+    def check_connection(self):
+       t = Thread(target=self.test_insert,args=(self.experiment,)) 
+       t.start()
+       t.join(2)
+       if t.is_alive():
+           raise Exception("Cannot cannot to MongoDB")
+
+    @staticmethod
+    def test_insert(experiment):
+        experiment.insert({"Test":1})
+        experiment.remove({"Test":1})
+if __name__ == '__main__':
+    database = Database()
+    database.client['test_db']['test_collection'].insert_one({"Test":"test"})
+    database.viewRun('test_db','test_collection')
+    database.removeRun('test_db','test_collection')
+    database.viewRun('test_db','test_collection')
+    
