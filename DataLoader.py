71d989a25bd61a333e1082be9a623797d0674ca9
Benson Li <bensonbinbinli@gmail.com>
2018-06-28 18:01:14 -0700

    finished getTable function. It will create a pandas dataframe from all the runs with the experimental parameters repeated, and thus Time will be the seperator for the values in the variable_name columns

--- DataLoader.py
+++ DataLoader.py
@@ -1,25 +1,31 @@
 from pymongo import MongoClient
 from LoggerUtils import *
 import pandas as pd
+import ipdb
 
-def getTable(folder_name):
+def getTable(run_name,folder_name='deep_learning'):
     '''
     returns a dataframe with the experiment parameters for each run in the folder
     '''
     mongo = Database()
-    folder = mongo.client[folder_name]
+    runs = mongo.client[folder_name][run_name]
     
     
-    for run in folder.collection_names():
-        experiment_parameters_iterator = folder[run].find({"Name of Text":{"$exists":"true"}})
-        variable_name_iterator = folder[run].find({"Variable name":{"$exists":"true"}})
-        for json_object in experiment_parameters_iterator:
-            print(json_object)
-    return json_object
+    document_iterator = runs.find()
+    # row_of_dicts=list(document_iterator)
+    df_list =[]
+    for json_object in document_iterator:
+        some_dict = {key: value for key, value in json_object.items() if key != '_id'}
+        some_df = pd.DataFrame(dict([ (k,pd.Series(v)) for k,v in some_dict.items() ]))
+        print(len(some_df))
+        df_list.append(some_df)
+    final_df = pd.concat(df_list,ignore_index=False)
+    ipdb.set_trace()
+    return final_df
         
 
 if __name__ == '__main__':
-    ob = getTable('aql')
+    ob = getTable('lunarlander')
 
 
 
