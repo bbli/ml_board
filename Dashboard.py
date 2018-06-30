5ade2a468841e502dac833f5e7a09fc3a5ac6165
Benson Li <bensonbinbinli@gmail.com>
2018-06-30 14:11:48 -0700

    started layout of dashboard

--- Dashboard.py
+++ Dashboard.py
@@ -2,7 +2,47 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+from DataLoader import getTable
+# import ipdb
+# import logging
+# logging.basicConfig(level=logging.DEBUG)
 
 app = dash.Dash(__name__)
+df = getTable('lunarlander')
+# Boostrap CSS.
+app.css.append_css({
+    "external_url": "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+})
 
+# Extra Dash styling.
+app.css.append_css({
+    "external_url": 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+})
 
+# JQuery is required for Bootstrap.
+app.scripts.append_script({
+    "external_url": "https://code.jquery.com/jquery-3.2.1.min.js"
+})
+
+# Bootstrap Javascript.
+app.scripts.append_script({
+    "external_url": "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+})
+################################################################
+
+num_graphs = len(df['Time'].unique())
+
+app.layout = html.Div([
+    html.Div(
+        [html.H1("Machine Learning Dashboard", className="text-center")]
+    ,className="row"),
+    html.Div(
+            [html.Div(
+                [dcc.Graph(id='a')]
+            ,className="col-md-8")]
+    ,className="row")
+    # +[html.Div([html.Div(html.Div(dcc.Graph(id=i)),className="col-md-8")])],className="row") for i in range(num_graphs)]
+], className="container-fluid")
+ 
+if __name__=='__main__':
+    app.run_server(debug=True)
