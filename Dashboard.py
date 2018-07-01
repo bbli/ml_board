cab78b02bf03aee4c7584064c2b11ffa09c0dfbd
Benson Li <bensonbinbinli@gmail.com>
2018-06-30 17:22:57 -0700

    dashboard now displays all the runs on each plot, albeit nothing is dynamic

--- Dashboard.py
+++ Dashboard.py
@@ -3,12 +3,13 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 from DataLoader import getTable
-# import ipdb
+import ipdb
 # import logging
 # logging.basicConfig(level=logging.DEBUG)
+# import plotly.graph_objs as go
+
 
 app = dash.Dash(__name__)
-df,var_names = getTable('lunarlander')
 # Boostrap CSS.
 app.css.append_css({
     "external_url": "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
@@ -31,20 +32,44 @@ app.scripts.append_script({
 ################################################################
 
 # num_graphs = len(df['Time'].unique())
+df,var_names = getTable('lunarlander')
+
+def createListOfButtonGraph(var_names):
+    html_div_list=[]
+    for var in var_names:
+        button = html.Div([html.Div(html.Button(var,id=var+'button'),className='col-md-8')],className="row")
+        html_div_list.append(button)
+
+        graph = html.Div([html.Div(dcc.Graph(id=var+'plot',figure=getFigure(var)),className="col-md-8")],className="row")
+        html_div_list.append(graph)
+    return html_div_list
+
+def getFigure(var):
+    '''
+    Gets the data for all the runs with the input variable name and plots them on one graph
+    '''
+    run_names = df['Time'].unique()
+    plot_for_each_run=[]
+    for run in run_names:
+        ##create dictionary
+        filtered_df=df[df.Time==run]
+        run_dict = {'y':list(filtered_df[var])}
+        plot_for_each_run.append(run_dict)
+    figure_dict= {'data':plot_for_each_run}
+    return figure_dict
+
 
 app.layout = html.Div([
     html.Div(
         [html.H1("Machine Learning Dashboard", className="text-center")]
     ,className="row")]+
-    [html.Div(
-        [html.Div(
-            [dcc.Graph(id='a'+str(i))]
-        ,className="col-md-8")]
-    ,className="row") for i in range(len(var_names))]
+    createListOfButtonGraph(var_names)
     # +[html.Div([html.Div(html.Div(dcc.Graph(id=i)),className="col-md-8")])],className="row") for i in range(num_graphs)]
 , className="container-fluid")
 
-# def table
+
  
 if __name__=='__main__':
     app.run_server(debug=True)
+    # div_list=createListOfButtonGraph(var_names)
+    # getFigure('Loss')
