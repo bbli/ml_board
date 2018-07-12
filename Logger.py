dbdd776b03f2abc3539a381d14e2a1660f417f21
Benson Li <bensonbinbinli@gmail.com>
2018-07-11 20:49:23 -0700

    began code for image logging

--- Logger.py
+++ Logger.py
@@ -2,7 +2,9 @@ from pymongo import MongoClient
 import datetime
 from LoggerUtils import *
 import ipdb
-
+from bson.binary import Binary
+# import cPickle
+import pickle
 
 class SummaryWriter(Database):
     def __init__(self,folder_name,run_name):
@@ -15,8 +17,12 @@ class SummaryWriter(Database):
 
     def add_scalar(self,variable_name:str, f:int):
         self.runs.update_one({"Experimental Parameters.Time":self.date},{'$push':{"Plots."+variable_name:f}},upsert= True)
-
-    def add_experiment_parameter(self,parameter_name:str, value:int):
+    def add_histogram(self, histogram_name:str, f:int):
+        self.runs.update_one({"Experimental Parameters.Time":self.date},{'$push':{"Histograms."+histogram_name:f}},upsert= True)
+    def add_image(self,image_name,image):
+        processed_image = Binary(pickle.dumps(image,protocol=2))
+        self.runs.update_one({"Experimental Parameters.Time":self.date},{'$push':{"Images."+image_name:processed_image}},upsert= True)
+    def add_experiment_parameter(self, parameter_name:str, value:int):
         self.runs.update_one({"Experimental Parameters.Time":self.date}, {'$set':{"Experimental Parameters."+parameter_name:value}})
     def viewRun(self):
         '''
